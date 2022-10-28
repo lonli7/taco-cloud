b18339bc5a1f41a8e81a1416f149e5d7f7350c79
lon <evens@bytensor.net>
2022-10-28 14:26:33 +0800

    init

---
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,15 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
